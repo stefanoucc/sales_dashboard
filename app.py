7af83ca69c9b4cd5141534f1ae5eb51aa9884d17
Stefano Uccelli <stefano.uccelli@kinanalytics.com>
2024-09-03 18:25:36 -0300

    corrected error

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ df['Payment Amount'] = pd.to_numeric(df['Payment Amount'], errors='coerce')
 
 # Convert 'Amount' to numeric
 df['Amount'] = df['Amount'].astype(int)
-"""  """
+df['Amount'].value_counts(dropna=False)
 # Sidebar Filters
 st.sidebar.header("Filters")
 product_filter = st.sidebar.multiselect("Select Product", df['Product'].unique())
